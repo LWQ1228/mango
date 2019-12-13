64d6b56b5c4d172ecefdf7c75ed090134a0afc4d
liuwenqing <15567725486@163.com>
2019-12-13 17:27:52 +0800

    添加ZipKin说明

---
+++ mango-zipkin/src/main/java/com/sunao/mango/zipkin/MangoZipkinApplication.java
@@ -0,0 +1,20 @@
+package com.sunao.mango.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * MangoZipkinApplication
+ *
+ * @author liuwenqing
+ * @blame 刘文清
+ * @date 2019/12/13 17:20
+ */
+@SpringBootApplication
+public class MangoZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangoZipkinApplication.class, args);
+    }
+
+}
---
+++ mango-zipkin/src/test/java/com/sunao/mango/zipkin/MangoZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunao.mango.zipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoZipkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
