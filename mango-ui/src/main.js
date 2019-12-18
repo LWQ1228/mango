16d065d9dcb2634a5edfa90cf8413acbcea3c9f8
liuwenqing <15567725486@163.com>
2019-12-18 11:47:34 +0800

    添加国际化处理

--- mango-ui/src/main.js
+++ mango-ui/src/main.js
@@ -4,9 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import api from './http'
+import i18n from './i18n'
 import global from './utils/global'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 //注册使用Element
 Vue.use(ElementUI)
@@ -18,6 +20,7 @@ Vue.prototype.global = global
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  i18n,
   router,
   render: h => h(App)
 })
